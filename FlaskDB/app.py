d09c16880c3c47570f487c04b09bc93988e6503f
Tiago Arrazi <tiago_arrazi98@outlook.com>
2019-05-31 21:24:01 -0300

    new mongodb endpoint and html view

--- FlaskDB/app.py
+++ FlaskDB/app.py
@@ -1,36 +1,22 @@
 from pymongo import MongoClient
 from pymysql import connect
-from flask import Flask
+from flask import Flask, render_template
 from json import dump
 import time
 
 app = Flask(__name__)  # flask application
 
-# ==================== MySQL connection ==================================
-
-mysql_conn = connect('localhost', 'banco_2', '123456', 'PROJETO_BANCO_2')  # MySQL connection
-
-# ==================== MongoDB client ====================================
-
-mongo_client = MongoClient('localhost', 27017)  # MongoDB client creation
-db = mongo_client['carros_db']  # MongoDB database selection
-carros = db['carros']
-
 
 @app.route('/mysql')
 def get_from_mysql():
+    mysql_conn = connect('localhost', 'banco_2', '123456', 'PROJETO_BANCO_2')  # MySQL connection
+
     with mysql_conn:
-        query = """SELECT C.MODELO AS 'MODELO CARRO',CRC.TIPO AS 'TIPO CARROCERIA',CRC.STATUS AS 'STATUS CARROCERIA',
-                   INTR.VOLANTE AS 'MODELO VOLANTE', INTR.BANCOS AS 'TIPO BANCO', TRNSM.TIPO AS 'TIPO TRANSMISSÃO'
-                   FROM CARRO AS C  
+
+        query = """SELECT C.MODELO AS 'MODELO CARRO',CRC.TIPO AS 'TIPO CARROCERIA',CRC.STATUS AS 'STATUS CARROCERIA'
+                   FROM CARRO AS C
                    INNER JOIN CARROCERIA AS CRC
-                   INNER JOIN INTERIOR AS INTR
-                   INNER JOIN TRANSMISSAO AS TRNSM
-                   WHERE (CRC.ID_CARROCERIA = C.ID_CARROCERIA AND (CRC.COR = 'VERMELHO' OR CRC.COR = 'PRATA' 
-                   OR CRC.COR = 'PRETO' OR CRC.COR = 'AZUL')) AND 
-                   (INTR.ID_INTERIOR = C.ID_INTERIOR AND (INTR.BANCOS = 'COURO' OR INTR.BANCOS = 'TECIDO')) AND
-                   (TRNSM.ID_TRANSMISSAO = C.ID_TRANSMISSAO AND 
-                   (TRNSM.TIPO = 'AUTOMATICO' OR TRNSM.TIPO = 'AUTOMATIZADO')) 
+                   WHERE (CRC.COR = 'VERMELHO' OR CRC.COR = 'PRATA') AND CRC.ID_CARROCERIA = C.ID_CARROCERIA
                    ORDER BY C.MODELO;"""
 
         cursor = mysql_conn.cursor()
@@ -38,13 +24,14 @@ def get_from_mysql():
         start = time.time()
 
         cursor.execute(query)
+
         response = cursor.fetchall()
 
         end = time.time()
 
         with open('mysql_response', 'w+') as mysql_resp:
             mysql_resp.write(
-                'MODELO | TIPO CARROCERIA | STATUS CARROCERIA | MODELO VOLANTE | TIPO BANCO | TIPO TRANSM.\n\n'
+                'MODELO | TIPO CARROCERIA | STATUS CARROCERIA\n\n'
             )
             for element in response:
                 mysql_resp.write("{}\n".format(str(element)))
@@ -55,33 +42,62 @@ def get_from_mysql():
 @app.route('/mongodb')
 def get_from_mongodb():
 
+    mongo_client = MongoClient('localhost', 27017)  # MongoDB client creation
+    db = mongo_client['carros_db']  # MongoDB database selection
+    carros = db['carros']
+
+    # cor_match = {"$match": {"$or": [{"carroceria.cor": "VERMELHO"},
+    #                                 {"carroceria.cor": "PRATA"}]
+    #                         }
+    #              }
+    #
+    # sort = {"$sort": {"modelo": 1}}
+    #
+    # project = {"$project": {
+    #     "_id": 0,
+    #     "MODELO CARRO": "$modelo",
+    #     "TIPO CARROCERIA": "$carroceria.tipo",
+    #     "STATUS CARROCERIA": "$carroceria.status"}}
+    #
+    # start = time.time()
+    #
+    # print('Executing aggregation...')
+    # results = list(carros.aggregate([cor_match, sort, project]))
+    #
+    # end = time.time()
+
+    # with open('mongodb_response.json', 'w+') as mongo_resp:
+    #     dump(list(results), mongo_resp)
+
+    # return '< AGGREGATION PROCESSED > --- {} documents in {} seconds'.format(len(results), end - start)
+
     cor_match = {"$match": {"$or": [
-                {"carroceria.cor": "VERMELHO"},
-                {"carroceria.cor": "PRATE"},
-                {"carroceria.cor": "AZUL"},
-                {"carroceria.cor": "PRETO"}]}}
+        {"carroceria.cor": "VERMELHO"},
+        {"carroceria.cor": "PRATA"}]}}
 
     sort = {"$sort": {"modelo": 1}}
-    limit = {"$limit": 10000}
+    count = {"$count": "contador"}
 
     project = {"$project": {
         "_id": 0,
         "MODELO CARRO": "$modelo",
         "TIPO CARROCERIA": "$carroceria.tipo",
-        "STATUS CARROCERIA": "$carroceria.status",
-        "MODELO VOLANTE": "$interior.volante",
-        "TIPO BANCO": "$interior.bancos"}}
+        "STATUS CARROCERIA": "$carroceria.status"}}
 
     start = time.time()
-
-    results = list(carros.aggregate([cor_match, sort, project]))
-
+    cursor = carros.aggregate([cor_match, sort, project])
     end = time.time()
+    tempo = end - start
 
-    with open('mongodb_response.json', 'w+') as mongo_resp:
-        dump(list(results), mongo_resp)
+    carros_html = carros.aggregate([cor_match, {"$limit": 10}, sort, project])
+    carros_count_doc = db.command({"aggregate": "carros",
+                                   "pipeline": [cor_match, sort, count],
+                                   "cursor": {}})
 
-    return '< QUERY PROCESSED > --- {} documents in {} seconds'.format(len(results), end - start)
+    return render_template('mongodb.html',
+                           carrosHtml=list(carros_html),
+                           carrosCount=carros_count_doc["cursor"]["firstBatch"][0]["contador"],
+                           time=tempo)
 
 
 if __name__ == '__main__':
